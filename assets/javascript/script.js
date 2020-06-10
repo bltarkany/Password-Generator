cabd491283064a3ddcfb5ca53df1651603588e93
Bobbi Tarkany <bl.tarkany@gmail.com>
2020-06-10 10:19:01 -0700

    pseudo coded initial function for grabbing password criteria. Completed character storage variables.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,10 +2,24 @@
 
 var spChar = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '[', ']', '<', '>', '?', '{', '}'];
 
-var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 
 var lowerChar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 var upperChar = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z'];
 
+// function that prompts for password criteria from user
+// has conditional statements so the password passes standard
 // Object that stores collected information from prompts
+
+function grabPasswordCriteria() {
+    // prompt password length in integer
+
+    // prompt for lowercase characters
+
+    // prompt for uppercase characters
+
+    // prompt for special characters
+
+    // prompt for numbers
+}
